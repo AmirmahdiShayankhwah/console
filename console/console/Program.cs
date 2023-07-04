66b483af5d08614085c7e4c7517c853b57a229e0
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2023-07-05 01:15:23 +0430

    console

---
+++ console/console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+
+            Console.WriteLine(stu.fname);
+            Console.WriteLine(stu.id);
+            Console.WriteLine(stu.lname);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ console/console/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Student
+    {
+        public int id = 20;
+        public string fname = "amir";
+        public string lname = "amiri";
+    }
+}
